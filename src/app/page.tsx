e57aac28af793c7c1bdca6daf6f43092564d2b37
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2025-12-31 17:22:11 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from '@/components/ui/button';
+'use client';
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
 
 const Page = () => {
+  const trpc = useTRPC();
+  const { data: users } = useQuery(trpc.getUsers.queryOptions());
   return (
-    <div>
-      Hello World <Button>Click me</Button>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <p>user.length: {users?.length}</p>
+        <ul className="list-disc list-inside">
+          {users?.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
