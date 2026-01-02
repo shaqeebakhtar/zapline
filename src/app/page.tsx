9fb5d036e4a53b3d08e6ba7656638177bebd718c
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2026-01-02 22:10:45 +0530

    setup inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
-import { requireAuth } from '@/lib/auth-utils';
-import { caller } from '@/trpc/server';
+'use client';
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       Protect page
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create Workflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,22 @@
 import prisma from '@/lib/db';
 import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findUnique({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: 'test@test.com',
       },
     });
+
+    return {
+      success: true,
+      message: 'Workflow created',
+    };
   }),
 });
 // export type definition of API
